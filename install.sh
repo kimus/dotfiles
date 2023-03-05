fe6db35a4286431ad61465e257c4bbb9023514e0
Helder Rossa <kimus.linuxus@gmail.com>
2023-03-05 22:24:35 +0000

    fixed a typo on install script

--- install.sh
+++ install.sh
@@ -22,12 +22,13 @@ fi
 BW_URL="https://vault.bitwarden.com/download/?app=cli&platform=linux"
 BW_FILE=$(mktemp)
 if [ "$(command -v curl)" ]; then
-    curl -L $BW_URL -o $BW_FILE
+    curl -fsL $BW_URL -o $BW_FILE
 elif [ "$(command -v wget)" ]; then
-    wget $BW_URL -O $BW_FILE
+    wget -q $BW_URL -O $BW_FILE
 fi
-unzip $BW_FILE "$HOME/.local/bin"
+unzip -o $BW_FILE -d "$HOME/.local/bin"
 rm $BW_FILE
+echo "Bitwarden CLI installed"
 export BW_SESSION=$(bw login $BW_EMAIL $BW_PASSWORD --raw)
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
